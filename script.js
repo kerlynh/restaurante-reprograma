d3ac1ce865ee655bf38f76aa9b35a9c827f6f73c
kerlynh <kerlynh@gmail.com>
2019-06-12 21:34:48 -0300

    inserir form

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 const container = document.querySelector('#items-cardapio')
-fetch(`http://localhost:3000/comidas`)
+fetch(`http://localhost:3000/comidas/`)
     .then((response) =>{
         return response.json();
     })
     .then((data) =>{
 
-        data.pratosFavoritos.forEach(prato => {
+        data.forEach(prato => {
             console.log(prato)
 
             const mediaItem = document.createElement('div');
@@ -26,3 +26,28 @@ fetch(`http://localhost:3000/comidas`)
     .catch((erro)=>{
         console.log(erro)
     })
+
+    // Para pegar imagens aleatorias:
+    // https://placeholder.com/
+    // https://picsum.photos/
+const botao = document.querySelector('#criar_comida_button')
+botao.addEventListener("click", criarComida)
+
+function criarComida () {
+  const nome = document.querySelector("#nome_input").value
+  const descricao = document.querySelector("#descricao_input").value
+  const imagem = document.querySelector("#imagem_input").value
+  const comida = {
+    nome, descricao, imagem
+  }
+  fetch(
+    'http://localhost:3000/comidas',
+    {
+      method: 'POST',
+      body: JSON.stringify(comida),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    }
+  ).then(response => console.log("criou!"))
+}
